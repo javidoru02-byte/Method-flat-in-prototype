f59461bdde8d069f2d81fe6ebceb006f90ddab68
David <javidoru02@gmail.com>
2026-05-04 15:34:56 +0300

    New code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,63 @@
 function MyFlat(arr) {
-  this.arr = arr;
+  this.arr = arr || [];
 }
 
-MyFlat.prototype.flats = function (depth = 1) {
-  function flatten(arr, depth) {
-    let result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-      if (i in arr === false) continue;
-
-      if (Array.isArray(arr[i]) && depth > 0) {
-        result.push(...flatten(arr[i], depth - 1));
-      } else {
-        result.push(arr[i]);
-      }
+MyFlat.prototype.forEach = function (callback) {
+  for (let i = 0; i < this.arr.length; i++) {
+    if (i in this.arr) {
+      callback(this.arr[i], i, this.arr);
     }
-
-    return result;
   }
-
-  return flatten(this.arr, depth);
 };
 
-const myArray = [1, 2, "    ", 3, [4, 5, [1, 4]]];
+MyFlat.prototype.concat = function (...args) {
+  const result = new MyFlat([]);
+  result.arr.push(...this.arr);
+
+  for (const arg of args) {
+    if (arg instanceof MyFlat) {
+      result.arr.push(...arg.arr);
+    } else if (Array.isArray(arg)) {
+      result.arr.push(...arg);
+    } else {
+      result.arr.push(arg);
+    }
+  }
+  return result;
+};
+
+MyFlat.prototype.flat = function (depth = 1) {
+  const flatten = (current, currentDepth) => {
+    const result = new MyFlat([]);
+
+    const wrapper = new MyFlat(current);
+
+    wrapper.forEach((item) => {
+      if ((item instanceof MyFlat || Array.isArray(item)) && currentDepth > 0) {
+        const nextDepth =
+          currentDepth === Infinity ? Infinity : currentDepth - 1;
+        const flattened = flatten(
+          item instanceof MyFlat ? item.arr : item,
+          nextDepth,
+        );
+
+        result.arr = result.concat(flattened).arr;
+      } else {
+        result.arr = result.concat([item]).arr;
+      }
+    });
+
+    return result;
+  };
+
+  return flatten(this.arr, depth).arr;
+};
+
+const myArray = [1, 2, " ", 3, [4, 5, [1, 4]], , 2];
 const a = new MyFlat(myArray);
 
-console.log(a.flats(Infinity));
+console.log("flat(Infinity):", a.flat(Infinity));
+
+console.log("flat(1):", a.flat(1));
+
+console.log("flat(2):", a.flat(2));
